9c0a235da192254bc9e4ebbbc6b7eeaba4c1cf36
hoonley <huyngo2001@gmail.com>
2024-10-01 01:00:44 -0700

    test commit

---
+++ public/firebase.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyBpRdNjtxVGiC6NCjt58-gnJUdvnbODXpc",
+    authDomain: "carjourney491b.firebaseapp.com",
+    projectId: "carjourney491b",
+    storageBucket: "carjourney491b.appspot.com",
+    messagingSenderId: "248223244957",
+    appId: "1:248223244957:web:4b1d289950719d829d6a74",
+    measurementId: "G-WF2R4YV2CS"
+};
--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,7 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
 // Image gallery images 
 const carImages = [
   "images/camry1.jpg", 
